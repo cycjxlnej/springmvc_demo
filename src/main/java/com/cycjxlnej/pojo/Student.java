b009091b3016441fe7616e385bbaedd16dafd17a
cycjxlnej <cycjxlnej@163.com>
2023-04-29 16:25:25 +0800

    初始化项目

---
+++ src/main/java/com/cycjxlnej/pojo/Student.java
@@ -0,0 +1,26 @@
+package com.cycjxlnej.pojo;
+
+/**
+ * @Author chenwei
+ * @Date 2023/4/29 4:18 下午
+ */
+public class Student {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
